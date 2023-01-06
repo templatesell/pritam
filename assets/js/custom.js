49adf9de7a41f590954f590f7548d776b7e0e3df
rameshkhanal <khanalramesh35@gmail.com>
2023-01-06 15:27:06 +0545

    unwanted js delete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,27 +3,6 @@
 (function($) {
 
 	"use strict";
-/*==============================================
-                      SIDE MENU
-    ===============================================*/
-
-    // $(".menu-btn").on("click", function(){
-    //   $(this).toggleClass("active");
-    //   $("body").toggleClass("no-scroll");
-    //   $(".side-menu").toggleClass("active");
-    // });
-
-    // $(".close-sidemenu").on("click", function(){
-    //   $(".side-menu").removeClass("active");
-    //   $(".menu-btn").removeClass("active");
-    // });
-    // $(".navigation ul, .side-menu .blog-items").parent().addClass("menu-item-has-children");
-    // $(".navigation li.menu-item-has-children > a").on("click", function() {
-    //   $(this).parent().toggleClass("active").siblings().removeClass("active");
-    //   $(this).next("ul, .blog-items").slideToggle();
-    //   $(this).parent().siblings().find("ul, .blog-items").slideUp();
-    //   return false;
-    // });
 
 
     /*==============================================
